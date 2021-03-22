ab5584e3104dead44e5186952b2cb170ddc88fe5
rvision0925 <huniu199531@gmail.com>
2021-03-21 22:51:56 -0500

    Update __init__.py

--- app/ib_insync/__init__.py
+++ app/ib_insync/__init__.py
@@ -25,7 +25,7 @@ from .contract import (
     TagValue, ComboLeg, DeltaNeutralContract, ContractDetails,
     ContractDescription, ScanData)
 from .order import (
-    Order, Trade, LimitOrder, MidPriceOrder, TrailingStopOrder, MarketOrder, StopOrder, StopLimitOrder,
+    Order, Trade, LimitOrder, MarketOrder, StopOrder, StopLimitOrder,
     BracketOrder, OrderCondition, ExecutionCondition, MarginCondition,
     TimeCondition, PriceCondition, PercentChangeCondition, VolumeCondition,
     OrderStatus, OrderState, OrderComboLeg)
